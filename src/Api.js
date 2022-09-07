4a5d05a6414f8b640fadcfcc1d49888c66d3a047
MrFatKatElder <104768030+MrFatKatElder@users.noreply.github.com>
2022-09-07 21:24:17 +0900

    Updates

---
+++ src/Api.js
@@ -0,0 +1,4 @@
+export default class Api {
+  static url = "https://api.openweathermap.org";
+  static key = "310211ce296edd42f161b4add8b84c37";
+}
---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      "The weather in ${response.data.main.name} is ${response.data.main.temp}°C"
+    );
+  }
+  let apiKey = "310211ce296edd42f161b4add8b84c37";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
+  g=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return <h2> Hello from weather</h2>;
+}
