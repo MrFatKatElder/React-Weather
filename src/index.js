c031bf12dc710204de1cde95f34da430fe6e5120
MrFatKatElder <104768030+MrFatKatElder@users.noreply.github.com>
2022-09-11 20:42:34 +0900

    Alter code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./Main";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDom.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.querySelector("#weather")
+import Weather from "./Weather";
+
+import "./Weather.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <div className="App">
+      <Weather />
+    </div>
+  </StrictMode>
 );
